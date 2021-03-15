e4f29f1cb8950f0b771ccc12606db9a4bac5794e
Lina Gorihvostka <lina.editor@gmail.com>
2021-03-15 16:23:41 +0300

    init: start reverse int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+  let reversedString = n.toString().split('').reverse().join('');
+  let reversedNumber = parseInt(parseFloat(reversedString));
+  return reversedNumber;
 }
